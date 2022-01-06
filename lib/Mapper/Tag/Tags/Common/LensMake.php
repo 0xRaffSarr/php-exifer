814d4a7eab659f3da9a56c9a76f9f9a66dba056d
Raffaele Sarracino <contacts@raffaelesarracino.it>
2022-01-06 16:27:00 +0100

    added LensMake Tag

---
+++ lib/Mapper/Tag/Tags/Common/LensMake.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * PHP Exifer Tag LensMake: Defines LensMake Tag.
+ *
+ * @link  https://github.com/0xRaffSarr/php-exifer
+ * @see https://exiftool.org/TagNames/EXIF.html
+ * @copyright Copyright (c) 2022. Raffaele Sarracino <contacts@raffaelesarracino.it>
+ * @license https://github.com/0xRaffSarr/php-exifer/blob/main/LICENSE
+ * @package Tag
+ */
+
+namespace Xraffsarr\PhpExifer\Mapper\Tag\Tags\Common;
+
+use Xraffsarr\PhpExifer\Mapper\MapperAbstract;
+use Xraffsarr\PhpExifer\Mapper\Tag\Tag;
+
+class LensMake extends Tag
+{
+    const ID = '0xA433';
+
+    const NAME = 'LensMake';
+    const SECTION = MapperAbstract::SECTION_EXIF;
+
+    /**
+     * @inheritDoc
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getValueDescription(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDescription(): ?string
+    {
+        return null;
+    }
+}
